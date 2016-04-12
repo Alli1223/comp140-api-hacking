ce7b73e2c58bbae74a76135c4aac1e53bfdfeba5
Alastair Rayner <alli0@me.com>
2016-04-12 19:43:53 +0100

    added a request for the weather from the API

--- Relic Game/scripts/Scripts/rainSpawn.cs
+++ Relic Game/scripts/Scripts/rainSpawn.cs
@@ -1,5 +1,7 @@
-﻿using UnityEngine;
+using UnityEngine;
 using System.Collections;
+using System.Xml;
+using System.Xml.Serialization;
 
 public class rainSpawn : MonoBehaviour
 {
@@ -10,12 +12,36 @@ public class rainSpawn : MonoBehaviour
 
 	private int rainSpawner;
 
+    public string location;
 
-	// Use this for initialization
-	void Start ()
+    private string APIText ;
+    public string APIurl;
+    public string APIHTTP = "http://api.openweathermap.org/data/2.5/weather?q=";
+    public string APIKey = "&APPID=d3dcece6b95e45b36bc819afc815e9ef";
+    public string APIFormat = "&mode=xml";
+
+
+
+    // Use this for initialization
+    void Start ()
 	{
-//		rain = GetComponentsInChildren<gameObject>();
-		rainSpawner = Random.Range(minTime,maxTime);
+        location = "Falmouth";
+
+        //Request XML
+        string APIurl = APIHTTP + location + APIKey + APIFormat;
+ 
+        WWW www = new WWW(APIurl);
+        APIText = www.text;
+
+        //Write XML
+
+
+        //readXML
+
+
+        /* Previous random version
+
+    rainSpawner = Random.Range(minTime,maxTime);
 		if (rainSpawner <= 2)
 		{
 			rain.SetActive(true);
@@ -24,6 +50,8 @@ public class rainSpawn : MonoBehaviour
 		{
 			rain.SetActive(false);
 		}
+
+    */
 
 	}
 	
