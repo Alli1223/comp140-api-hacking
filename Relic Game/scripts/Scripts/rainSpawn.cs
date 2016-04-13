6a3b31b25a26760150f77be12c11b6147a87af07
Alastair Rayner <alli0@me.com>
2016-04-13 17:38:19 +0100

    wrapped the get request in a function

--- Relic Game/scripts/Scripts/rainSpawn.cs
+++ Relic Game/scripts/Scripts/rainSpawn.cs
@@ -1,7 +1,10 @@
-using UnityEngine;
+﻿using UnityEngine;
 using System.Collections;
 using System.Xml;
 using System.Xml.Serialization;
+using System.Text;
+using System.IO;
+
 
 public class rainSpawn : MonoBehaviour
 {
@@ -12,52 +15,83 @@ public class rainSpawn : MonoBehaviour
 
 	private int rainSpawner;
 
-    public string location;
+    
 
-    private string APIText ;
     public string APIurl;
-    public string APIHTTP = "http://api.openweathermap.org/data/2.5/weather?q=";
-    public string APIKey = "&APPID=d3dcece6b95e45b36bc819afc815e9ef";
-    public string APIFormat = "&mode=xml";
 
+    public string APIHTTP = "http://api.openweathermap.org/data/2.5/weather?q=";
+    public string location = "Falmouth";
+    public string APIKey = "&APPID=d3dcece6b95e45b36bc819afc815e9ef";   //My personal API key that allows for upto 600 calls an hour
+    public string APIUnits = "&units=metric"; //API unit standard - Metric or Imperial
+    public string APIFormat = "";  //XML format &mode=xml
 
 
     // Use this for initialization
     void Start ()
 	{
-        location = "Falmouth";
+        
+        //Create XML document
+        XmlDocument xmlDoc = new XmlDocument();
 
-        //Request XML
-        string APIurl = APIHTTP + location + APIKey + APIFormat;
- 
-        WWW www = new WWW(APIurl);
-        APIText = www.text;
 
-        //Write XML
+        //Concatinated XML Request
+        string APIurl = APIHTTP + location + APIKey + APIUnits + APIFormat;
+        Debug.Log(APIurl);
+
+        string API_Content = getAPIContent(APIurl);
+
+
+        string raintext = API_Content.Substring(0, API_Content.IndexOf("RAIN"));
+
+        Debug.Log(API_Content);
+        if (raintext.Contains("RAIN"))
+        {
+            rain.SetActive(true);
+        }
+        else
+            rain.SetActive(false);
+
+
+        //write xml function
+        //WriteXML(API_Content);
 
 
         //readXML
+    }
 
 
-        /* Previous random version
+    public string getAPIContent(string APIurl)
+    {
+        WWW www = new WWW(APIurl);
+        string APIcontent = www.text;
+        
+        return APIcontent;
+    }
 
-    rainSpawner = Random.Range(minTime,maxTime);
-		if (rainSpawner <= 2)
-		{
-			rain.SetActive(true);
-		}
-		if (rainSpawner > 2)
-		{
-			rain.SetActive(false);
-		}
+    /*Write XML
+    public void WriteXML(string API_Content)
+    {
+        Debug.Log(API_Content);
+        string raintext = API_Content.Substring(0, API_Content.IndexOf("RAIN"));
 
+        if(raintext.Contains("RAIN"))
+        {
+            rain.SetActive(true);
+        }
+        else
+            rain.SetActive(false);
+    }
     */
 
-	}
-	
-	// Update is called once per frame
-	void Update ()
+
+
+    // Update is called once per frame
+    void Update ()
 	{
 	
 	}
+
+
 }
+
+
