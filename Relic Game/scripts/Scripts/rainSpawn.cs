4f5f02a76f432033ebd8a273ff83f27fb32198eb
Alastair Rayner <alli0@me.com>
2016-04-14 15:59:20 +0100

    Implemented a weather update system
    the weather now updates every 10 seconds

--- Relic Game/scripts/Scripts/rainSpawn.cs
+++ Relic Game/scripts/Scripts/rainSpawn.cs
@@ -9,13 +9,7 @@ using System.IO;
 public class rainSpawn : MonoBehaviour
 {
 
-    public GameObject rain;
-    private int minTime = 1;
-    private int maxTime = 4;
-
-    private int rainSpawner;
-
-
+	public GameObject rain;
 
     public string APIurl;
 
@@ -24,53 +18,68 @@ public class rainSpawn : MonoBehaviour
     public string APIKey = "&APPID=d3dcece6b95e45b36bc819afc815e9ef";   //My personal API key that allows for upto 600 calls an hour
     public string APIUnits = "&units=metric"; //API unit standard - Metric or Imperial
     public string APIFormat = "";  //return format (default is string text)
-
+    bool isWaiting;
 
     // Use this for initialization
     void Start()
     {
 
-        string url = APIHTTP + location + APIKey + APIUnits;
-        WWW www = new WWW(url);
-        StartCoroutine(WaitForRequest(www));
-
+            string url = APIHTTP + location + APIKey + APIUnits;
+            WWW www = new WWW(url);
+            //StartCoroutine(GetWeather(www));
+            StartCoroutine(UpdateWeather(10.0f, www));
     }
 
 
-    IEnumerator WaitForRequest(WWW www)
+    IEnumerator GetWeather(WWW www)
     {
+
         yield return www;
 
-        string API_Content = www.data;
+        string API_Content = www.text;
 
-        if (API_Content.Contains("RAIN"))
+        if (API_Content.Contains("Rain"))
         {
             rain.SetActive(true);
+            Debug.Log("It is Raining");
         }
         else
         {
             rain.SetActive(false);
+            Debug.Log("It is not Raining");
         }
-
 
         // check for errors
         if (www.error == null)
         {
-            Debug.Log("Request Recieved!: " + www.data);
+            Debug.Log("Weather Request Recieved!: " + www.text);
         }
-        else {
+        else
+        {
             Debug.Log("WWW Error: " + www.error);
         }
+        
     }
 
-
-
-
-    // Update is called once per frame
-    void Update()
+    IEnumerator UpdateWeather(float waitTime, WWW www)
     {
-
+        while (true)
+        {
+            yield return new WaitForSeconds(waitTime);
+            StartCoroutine(GetWeather(www));
+        }
     }
+
+
+
+
+
+
+        // Update is called once per frame
+        void Update ()
+	{
+	
+	}
 
 
 }
