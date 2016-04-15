e76859b0d413dc3bca7c7cb9aad3f155e1af3e86
Alastair Rayner <alli0@me.com>
2016-04-15 21:23:23 +0100

    almost done, need to fix issue where location isnt getting read by the getWeather function

--- Relic Game/scripts/Scripts/rainSpawn.cs
+++ Relic Game/scripts/Scripts/rainSpawn.cs
@@ -13,36 +13,47 @@ public class rainSpawn : MonoBehaviour
 
 
     private string APIHTTP = "api.openweathermap.org/data/2.5/weather?q=";
-    public string location = "";
     private string APIKey = "&APPID=d3dcece6b95e45b36bc819afc815e9ef";   //My personal API key that allows for upto 600 calls an hour
     private string APIUnits = "&units=metric"; //API unit standard - Metric or Imperial
     private string APIFormat = "";  //return format (default is string text)
     private float update_time = 10f; //Update time in seconds
-
+    public string location = "Bath";
 
     // Use this for initialization
     void Start()
     {
-        string API_IP_URL = "api.ipify.org";
-        WWW API_IP = new WWW(API_IP_URL);
 
-        //string url = APIHTTP + location + APIKey + APIUnits;
-        //WWW www = new WWW(url);
+        StartCoroutine(UpdateWeather(update_time));
+
+    }
 
 
-        StartCoroutine(UpdateWeather(update_time, API_IP));
+    //Constantly calls the function GetWeather then waits a set amnmount of time
+    IEnumerator UpdateWeather(float update_time)
+    {
+        while (true)
+        {
+            StartCoroutine(GETIP());
+            yield return new WaitForSeconds(update_time);
+        }
     }
 
     //If the weather API request contains the word "Rain" inside then set the rain animation to true, else don't
-
-    IEnumerator GetWeather()
+    IEnumerator GetWeather(string location)
     {
-        string url = APIHTTP + location + APIKey + APIUnits;
-        WWW www = new WWW(url);
+        //TODO: FIX THIS TO USE THE GENERATED LOCATION
+        
+        location = "Bath";
+        //Weird unity issue?
 
+        string url = APIHTTP + location + APIKey + APIUnits;
+        Debug.Log(location);
+        WWW www = new WWW(url);
+        
         yield return www;
 
         string API_Content = www.text;
+
 
         if (API_Content.Contains("Rain"))
         {
@@ -54,7 +65,7 @@ public class rainSpawn : MonoBehaviour
             rain.SetActive(false);
             Debug.Log("It is not Raining");
         }
-
+        
         // check for errors
         if (www.error == null)
         {
@@ -64,27 +75,17 @@ public class rainSpawn : MonoBehaviour
         {
             Debug.Log("WWW Error: " + www.error);
         }
-
     }
 
-    //Constantly calls the function GetWeather then waits a set amnmount of time
-    IEnumerator UpdateWeather(float update_time, WWW API_URL)
-    {
-        while (true)
-        {
-            StartCoroutine(GETIP(API_URL));
-            StartCoroutine(GetWeather());
-
-            yield return new WaitForSeconds(update_time);
-        }
-    }
-
-
+    
     //Returns the IP address of the user
-    IEnumerator GETIP(WWW API_URL)
+    IEnumerator GETIP()
     {
-        yield return API_URL;
-        string IP_Address = API_URL.text;
+        string API_IP_URL = "api.ipify.org";
+        WWW API_IP = new WWW(API_IP_URL);
+        
+        yield return API_IP;
+        string IP_Address = API_IP.text;
 
         //Starts another Coroutine to get the location based on that IP
         StartCoroutine(GetLocation(IP_Address));
@@ -93,14 +94,15 @@ public class rainSpawn : MonoBehaviour
 
     IEnumerator GetLocation(string IP_Address)
     {
-        string IP_Location_URL_start = "http://ipinfo.io/";
+        string IP_Location_URL_start = "http://ipinfo.io/"; //1000 Daily requests allowed for free
         string IP_Location_URL_end = "/city";
         string IP_Location_URL = IP_Location_URL_start + IP_Address + IP_Location_URL_end;
         WWW IP_Location = new WWW(IP_Location_URL);
-        Debug.Log(location);
         yield return IP_Location;
-        
-        
+
+        string location = IP_Location.text;
+
+        StartCoroutine(GetWeather(location));
     }
 
     // Update is called once per frame
