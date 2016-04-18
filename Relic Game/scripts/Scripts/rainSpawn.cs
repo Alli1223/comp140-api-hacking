e35303eec64f3b693910b780c15f97d72e3e2f84
Alastair Rayner <alli0@me.com>
2016-04-18 14:56:53 +0100

    refactored the layout of the code to be more linear
    moved the coroutines around to be in the order in which they are called

--- Relic Game/scripts/Scripts/rainSpawn.cs
+++ Relic Game/scripts/Scripts/rainSpawn.cs
@@ -11,20 +11,17 @@ public class rainSpawn : MonoBehaviour
 
     public GameObject rain;
 
-
     private string APIHTTP = "api.openweathermap.org/data/2.5/weather?q=";
     private string APIKey = "&APPID=d3dcece6b95e45b36bc819afc815e9ef";   //My personal API key that allows for upto 600 calls an hour
     private string APIUnits = "&units=metric"; //API unit standard - Metric or Imperial
-    private string APIFormat = "";  //return format (default is string text)
+    private string APIFormat = "";  //return format (default is string text)(&mode=xml for XML)
     private float update_time = 10f; //Update time in seconds
-    public string location = "Bath";
+    public string location;
 
     // Use this for initialization
     void Start()
     {
-
         StartCoroutine(UpdateWeather(update_time));
-
     }
 
 
@@ -37,47 +34,7 @@ public class rainSpawn : MonoBehaviour
             yield return new WaitForSeconds(update_time);
         }
     }
-
-    //If the weather API request contains the word "Rain" inside then set the rain animation to true, else don't
-    IEnumerator GetWeather(string location)
-    {
-        //TODO: FIX THIS TO USE THE GENERATED LOCATION
         
-        location = "Bath";
-        //Weird unity issue?
-
-        string url = APIHTTP + location + APIKey + APIUnits;
-        Debug.Log(location);
-        WWW www = new WWW(url);
-        
-        yield return www;
-
-        string API_Content = www.text;
-
-
-        if (API_Content.Contains("Rain"))
-        {
-            rain.SetActive(true);
-            Debug.Log("It is Raining");
-        }
-        else
-        {
-            rain.SetActive(false);
-            Debug.Log("It is not Raining");
-        }
-        
-        // check for errors
-        if (www.error == null)
-        {
-            Debug.Log("Weather Request Recieved!: " + www.text);
-        }
-        else
-        {
-            Debug.Log("WWW Error: " + www.error);
-        }
-    }
-
-    
     //Returns the IP address of the user
     IEnumerator GETIP()
     {
@@ -103,6 +60,53 @@ public class rainSpawn : MonoBehaviour
         string location = IP_Location.text;
 
         StartCoroutine(GetWeather(location));
+    }
+
+    IEnumerator GetWeather(string location)
+    {
+        //TODO: FIX THIS TO USE THE GENERATED LOCATION#
+        Debug.Log(location);
+        if (location.Contains("Falmouth"))
+        {
+            location = "Falmouth";
+        }
+        else
+        {
+            location = "somewhereelse";
+        }
+        Debug.Log(location);
+
+        string url = APIHTTP + location + APIKey + APIUnits + APIFormat;
+        WWW www = new WWW(url);
+
+        yield return www;
+
+        string API_Content = www.text;
+
+        //If the weather API request contains the word "Rain" inside then set the rain animation to true
+        //TODO: make this more robust by only checking the main weather condition
+        if (API_Content.Contains("Rain"))
+        {
+            rain.SetActive(true);
+            Debug.Log("It is Raining");
+        }
+        else
+        {
+            rain.SetActive(false);
+            Debug.Log("It is not Raining");
+        }
+
+
+        // check for errors
+        if (www.error == null)
+        {
+            Debug.Log("Weather Request Recieved!: " + www.text);
+        }
+        else
+        {
+            Debug.Log("WWW Error: " + www.error);
+        }
+
     }
 
     // Update is called once per frame
